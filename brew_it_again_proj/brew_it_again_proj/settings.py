6036b8a1531549b36c06b805fdb3555aede38027
known <mondo.beyondo3@gmail.com>
2024-05-22 00:08:11 -0400

    Update and successfully connect with PostgreSQL database

--- brew_it_again_proj/brew_it_again_proj/settings.py
+++ brew_it_again_proj/brew_it_again_proj/settings.py
@@ -21,13 +21,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -76,19 +71,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'brew_it_again_proj.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'brew_it_again_data',
+        'OPTIONS': {
+            'service': 'brew_service',
+            'passfile': '.brew_pgpass',
+        },
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -105,9 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
