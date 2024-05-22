d839d0372c35a595a9412cab6a2d5416cbb897b8
known <mondo.beyondo3@gmail.com>
2024-05-22 00:29:53 -0400

    Connect brew_it_again to main routing

--- brew_it_again_proj/brew_it_again_proj/urls.py
+++ brew_it_again_proj/brew_it_again_proj/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("brew_it_again/", include("brew_it_again.urls")),
     path('admin/', admin.site.urls),
 ]
