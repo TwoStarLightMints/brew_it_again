35de444150522effad4c522ba418a9f83ee4fb2c
known <mondo.beyondo3@gmail.com>
2024-07-27 11:16:37 -0400

    Remove the posibility of passing multiple allowed hosts due to a quirk with getting the environment variable and splitting it

--- bia_proj/bia_proj/settings.py
+++ bia_proj/bia_proj/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
+print(os.environ.get('ALLOWED_HOST'))
+
+ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOST') ]
 
 
 # Application definition
