88063affe2c824296cb8b101e7bff3684ecabb6b
known <mondo.beyondo3@gmail.com>
2024-07-26 23:38:38 -0400

    Add logic to calculate the necessary amount of substance required given the amount of one substance

--- bia_proj/coffee_ratio_calculator/static/calculator.js
+++ bia_proj/coffee_ratio_calculator/static/calculator.js
@@ -1,90 +1,45 @@
-const unit_selector = document.querySelector("#unit");
-const water_radio = document.querySelector("#water-radio");
-const coffee_radio = document.querySelector("#coffee-radio");
-const calc_display = document.querySelector('#display');
-const clear_button = document.querySelector('#clear');
-const del_button = document.querySelector('#delete');
+import { validate_ratio_input, validate_float_input } from '/static/utils.js';
+
 const calc_button = document.querySelector('#calculate');
-const num_buttons = document.querySelectorAll('.number');
+const result_paragraph = document.querySelector('p.result');
+const result_span = document.querySelector('span.result');
+const input_ratio = document.querySelector('#input-ratio')
+const input_amount = document.querySelector('#input-amount');
+const input_substance = document.querySelector('#w-or-c-label');
+const output_substance = document.querySelector('#w-or-c-out');
+const substance_in_type_radio = document.querySelectorAll('input[name="w-or-c"]');
 
-// True == water | False == coffee
-let curr_unit = true;
+Array.from(substance_in_type_radio).forEach(elem => elem.addEventListener('change', e => {
+    const substance_in_type_val = document.querySelector('input[name="w-or-c"]:checked').value;
 
-function load_unit_opts() {
-    function clear_unit_opts() {
-        Array.from(unit_selector.children).forEach(e => unit_selector.remove(e));
-    }
+    input_substance.textContent = substance_in_type_val;
 
-    if (curr_unit) {
-        clear_unit_opts();
-
-        water_units.forEach(e => {
-            unit_selector.appendChild(e);
-        });
-
-        unit_selector.value = water_units[0].value;
-    } else {
-        clear_unit_opts();
-
-        coffee_units.forEach(e => {
-            unit_selector.appendChild(e);
-        });
-
-        unit_selector.value = coffee_units[0].value;
-    }
-
-    curr_unit = !curr_unit;
-}
-
-// The units which will appear in the drop down will be stored and managed here
-const coffee_units = [
-    Object.assign(document.createElement('option'), {value: 'ounces', textContent: 'Ounces'}),
-    Object.assign(document.createElement('option'), {value: 'grams', textContent: 'Grams'}),
-];
-
-const water_units = [
-    Object.assign(document.createElement('option'), {value: 'fluid-ounces', textContent: 'Fluid Ounces'}),
-    Object.assign(document.createElement('option'), {value: 'milliliters', textContent: 'Milliliters'}),
-];
-
-water_radio.addEventListener("change", ev => { load_unit_opts(); } );
-coffee_radio.addEventListener("change", ev => { load_unit_opts(); } );
-
-// Load initial unit options
-load_unit_opts();
-
-Array.from(num_buttons).forEach(e => {
-    e.addEventListener("click", ev => {
-        const curr_content = calc_display.textContent;
-        const this_button = ev.target;
-
-        if (curr_content.length == 1 && curr_content[0] == "0" && this_button != ".")
-            calc_display.textContent = this_button.textContent;
-        else if (this_button == "." && !curr_content.includes("."))
-            ;
-    });
-});
-
-function clear_calculator() {
-    calc_display.textContent = '0';
-}
-
-function delete_calculator() {
-
-}
+    result_paragraph.style.display = 'none';
+    result_paragraph.setAttribute('hidden', "");
+    result_span.setAttribute('hidden', "");
+    output_substance.setAttribute('hidden', "");
+}))
 
 function calculate() {
+    const [ ratio_num, ratio_den ] = input_ratio.value.split("/").map(n => parseFloat(n));
+    const substance_amount = parseFloat(input_amount.value);
+    const substance_in_type_val = document.querySelector('input[name="w-or-c"]:checked').value;
 
+    if (substance_in_type_val === "water") {
+        result_span.textContent = ((substance_amount * ratio_num) / ratio_den).toFixed(2);
+        output_substance.textContent = "coffee";
+    } else if (substance_in_type_val === "coffee") {
+        result_span.textContent = ((substance_amount * ratio_den) / ratio_num).toFixed(2);
+        output_substance.textContent = "water";
+    }
+    
+    result_paragraph.style.display = 'block';
+    result_paragraph.removeAttribute('hidden');
+    result_span.removeAttribute('hidden');
+    output_substance.removeAttribute('hidden');
 }
 
-clear_button.addEventListener('click', e => {
-    clear_calculator();
-});
-
-del_button.addEventListener('click', e => {
-    delete_calculator();
-});
-
 calc_button.addEventListener('click', e => {
+    e.preventDefault();
     calculate();
 });
