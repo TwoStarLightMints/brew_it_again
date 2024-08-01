2aced82aee43debad48bcd58f35665f873c1ee16
known <mondo.beyondo3@gmail.com>
2024-08-01 13:43:30 -0400

    Update coffee recipe entry to use float field

--- bia_proj/bia/models.py
+++ bia_proj/bia/models.py
@@ -9,6 +9,6 @@ class CoffeeRecipeEntry (models.Model):
     title = models.CharField(max_length=40)
     grinder_setting = models.IntegerField()
     ratio = models.CharField(max_length=6)
-    grams_coffee = models.IntegerField()
-    mili_water = models.IntegerField()
+    grams_coffee = models.FloatField(max_length=12)
+    mili_water = models.FloatField(max_length=12)
     brew_time = models.CharField(max_length=6)
